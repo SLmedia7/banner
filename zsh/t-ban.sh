1a2d4cf6cb7f2682958c1ab6b1945e81c5d68136
SLmedia <65019639+SLmedia7@users.noreply.github.com>
2020-07-07 13:12:01 +0100

    Update t-ban.sh

--- zsh/t-ban.sh
+++ zsh/t-ban.sh
@@ -12,9 +12,9 @@ cyan='\e[0;36m'
 lightgreen='\e[1;32m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-echo -e $lightgreen "\e[1m                Security Help For Ethical Hackers... "
+echo 
 echo " "
-echo -e $yellow "\e[1m                         -sl media "
+echo 
 echo " "
 echo " "
 echo " "
